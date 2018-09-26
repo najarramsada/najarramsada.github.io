b4d12fff16e4bbaa8c9030a74a46c83323b48b15
najarramsada <40881620+najarramsada@users.noreply.github.com>
2018-09-26 13:45:50 +1000

    CSS Fix

--- css/app.js
+++ css/app.js
@@ -1,4 +1,19 @@
-{
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+  {
   "particles": {
     "number": {
       "value": 384,
@@ -108,3 +123,4 @@
   },
   "retina_detect": true
 }
+);
